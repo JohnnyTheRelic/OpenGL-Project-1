140b09c5d5c22e2b8fc95d6e91c3357253057741
Eric Wen <ew21hi@gmail.com>
2026-05-25 22:47:13 -0400

    water and flip

--- main.cpp
+++ main.cpp
@@ -97,7 +97,9 @@ int main()
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	int img_width, img_height, numColCh;
-	unsigned char* bytes = stbi_load("dirtmc.png", &img_width, &img_height, &numColCh, 0);
+
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("water.jpg", &img_width, &img_height, &numColCh, 0);
 	if (!bytes) {
 		std::cout << "no texture loaded" << std::endl;
 	}
@@ -112,7 +114,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img_width, img_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width, img_height, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
 	glGenerateMipmap(GL_TEXTURE_2D);
 
 	stbi_image_free(bytes);
