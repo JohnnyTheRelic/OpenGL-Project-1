ddb06fdbbd5bb82d6e2c4aa3a484511e9becb4e6
Eric Wen <ew21hi@gmail.com>
2026-06-08 23:31:55 -0400

    added lighting

--- Camera.cpp
+++ Camera.cpp
@@ -7,16 +7,22 @@ Camera::Camera(int width, int height, glm::vec3 position) {
 
 }
 
-void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform) {
+void Camera::Matrix(Shader& shader, const char* uniform) {
 
 	// return projection * view matrix
 
-	glm::mat4 view  = glm::mat4(1.0f);
+	glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(cameraMatrix));
+
+	
+}
+
+void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane) {
+	glm::mat4 view = glm::mat4(1.0f);
 	glm::mat4 projection = glm::mat4(1.0f);
 	view = glm::lookAt(Position, Position + Orientation, Up);
 	projection = glm::perspective(glm::radians(FOVdeg), (float)(width / height), nearPlane, farPlane);
+	cameraMatrix = projection * view;
 
-	glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(projection * view));
 }
 void Camera::Inputs(GLFWwindow* window) {
 
