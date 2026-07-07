441abaab36be6d4c3e0515641fca4ac7a649c47f
Eric Wen <ew21hi@gmail.com>
2026-07-06 23:48:56 -0400

    getIndices

--- Model.cpp
+++ Model.cpp
@@ -17,11 +17,71 @@ std::vector<unsigned char> Model::getData() {
 }
 
 std::vector<float> Model::getFloats(json accessor) {
-	return {};
+	std::vector<float> floatVec;
+	unsigned int buffViewInd = accessor.value("bufferView", 1);
+	unsigned int count = accessor["count"];
+	unsigned int accByteOffset = accessor.value("byteOffset", 0);
+	std::string type = accessor["type"];
+
+	json bufferView = JSON["bufferViews"][buffViewInd];
+	unsigned int byteOffset = bufferView["byteOffset"];
+
+	unsigned int numPerVert;
+	if (type == "SCALAR") numPerVert = 1;
+	else if (type == "VEC2") numPerVert = 2;
+	else if (type == "VEC3") numPerVert = 3;
+	else if (type == "VEC4") numPerVert = 4;
+	else throw std::invalid_argument("Type is invalid (not SCALAR, VEC2, VEC3, or VEC4)");
+	unsigned int beginningOfData = byteOffset + accByteOffset;
+	unsigned int lengthOfData = count * 4 * numPerVert;
+	for (unsigned int i = beginningOfData; i < beginningOfData + lengthOfData; i) {
+		unsigned char bytes[] = { data[i++],data[i++],data[i++],data[i++] };
+		float value;
+		std::memcpy(&value, bytes, sizeof(float));
+		floatVec.push_back(value);
+	}
+	return floatVec;
 }
 
 std::vector<GLuint> Model::getIndices(json accessor) {
-	return {};
+	std::vector<GLuint> indices;
+	unsigned int buffViewInd = accessor.value("bufferView", 1);
+	unsigned int count = accessor["count"];
+	unsigned int accByteOffset = accessor.value("byteOffset", 0);
+	unsigned int componentType = accessor["componentType"];
+
+	json bufferView = JSON["bufferViews"][buffViewInd];
+	unsigned int byteOffset = bufferView["byteOffset"];
+
+	unsigned int beginningOfData = byteOffset + accByteOffset;
+	if (componentType == 5125) {
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 4; i) {
+			unsigned char bytes[] = { data[i++],data[i++],data[i++] ,data[i++] };
+			unsigned int value;
+			std::memcpy(&value, bytes, sizeof(unsigned int));
+			indices.push_back((GLuint)value);
+			
+		}
+	}
+	else if (componentType == 5123) {
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 2; i) {
+			unsigned char bytes[] = { data[i++],data[i++] };
+			unsigned short value;
+			std::memcpy(&value, bytes, sizeof(unsigned short));
+			indices.push_back((GLuint)value);
+		}
+	}
+	else if (componentType == 5122) {
+		for (unsigned int i = beginningOfData; i < byteOffset + accByteOffset + count * 2; i) {
+			unsigned char bytes[] = { data[i++],data[i++] };
+			short value;
+			std::memcpy(&value, bytes, sizeof(short));
+			indices.push_back((GLuint)value);
+		}
+		
+
+	}
+	return indices;
 }
 
 std::vector<glm::vec2> Model::groupFloatsVec2(std::vector<float> floatVec) {
